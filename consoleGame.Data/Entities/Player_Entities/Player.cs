57c496333b60e945752be7ecaccf1d152c4597b4
Kody Terew <terewkody@gmail.com>
2023-04-27 17:41:00 -0400

    Minor Changes

--- consoleGame.Data/Entities/Player_Entities/Player.cs
+++ consoleGame.Data/Entities/Player_Entities/Player.cs
@@ -1,11 +1,25 @@
 
 public class Player
 {
-    public string Name = string.Empty();
+    public Player(string name)
+    {
+        Name = name;
+    }
 
+public int ID {get;set;}
+    public string Name {get;set;}= string.Empty;
     public int healthPoints {get; set;} = 100;
+
     public List<Items> Bag;
     
+    public void DecreaseHealth(int value)
+    {
+        healthPoints -= value;
+    }
+    public void IncreaseHealth(int value)
+    {
+        healthPoints += value;
+    }
     public bool IsDead
     {
         get
@@ -14,5 +28,14 @@ public class Player
         }
     }
 
-
+    private void UseWeapon(Enemy enemy, int attackPower, int accuracy)
+    {
+        Random rand = new Random();
+        int number = rand.Next(0,99);
+        if(accuracy > number)
+        {
+            
+            System.Console.WriteLine();
+        }
+    }
 }
