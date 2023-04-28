db3881fcb2e22475480663268b6b8e62f3e941fd
Kody Terew <terewkody@gmail.com>
2023-04-27 21:27:43 -0400

    Added EnemyRepository

---
+++ consoleGame.Repository/Enemy_Repository/EnemyRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace consoleGame.Repository.Enemy_Repository
+{
+    public class EnemyRepository
+    {
+        
+    }
+}
