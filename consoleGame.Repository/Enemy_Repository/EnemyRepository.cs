79371097c4d37020d7ef4e4d08fe15e04484e80e
Kody Terew <terewkody@gmail.com>
2023-04-27 21:29:44 -0400

    Enemy Repository update

--- consoleGame.Repository/Enemy_Repository/EnemyRepository.cs
+++ consoleGame.Repository/Enemy_Repository/EnemyRepository.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace consoleGame.Repository.Enemy_Repository
+public class EnemyRepository
 {
-    public class EnemyRepository
-    {
-        
-    }
+    private List<Enemy> _enemyDB = new List<Enemy>();
+
+    private int _count = 0;
+
+    
 }
